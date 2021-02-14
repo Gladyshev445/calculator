28161e353441cf1ec33c18491156f5073db530b3
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-02-14 19:13:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let x = prompt("Выбирайте первое число")
+let y = prompt("Выбирайте второе число")
+
+alert("Проходит процесс")
+console.log(x + y)
+console.log(x - y)
+console.log(x * y)
+let z = x+y
+    e = x-y
+    r = x*y
+    t = x**y
+    u = x%y
+    alert("Ваше + число " + z)
+    alert("Ваше - число " + e)
+    alert("Ваше * число " + r)
+    alert("Ваше ** число " + t)
+    alert("Ваше % число " + u)
+    alert("Процессы записаны на f12")
